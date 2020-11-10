9393cbd183355e31b5d842c952b78b0fedac7ff7
Akhil Murthy <akhil.s.murthy@gmail.com>
2020-11-10 14:14:43 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Akhil Murthy on 11/10/20.
+//  Copyright © 2020 Akhil Murthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
